d88d60f32355f11f547d69ff3b88ea92fbd38898
Hamer Garcia <hamgar28@gmail.com>
2025-04-07 01:33:16 -0500

    discovery server added

--- BackEnd/user/src/main/java/com/wartinder/user/UserApplication.java
+++ BackEnd/user/src/main/java/com/wartinder/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.wartinder.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UserApplication {
 
